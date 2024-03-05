1219fb090311eea22a84826a03148b57710900f3
albertomoiseisdev <contatoalbertomoiseis@gmail.com>
2024-03-05 18:05:22 -0300

    att: Efeito no números.

--- js/efeito.js
+++ js/efeito.js
@@ -1,9 +1,45 @@
-var btn = document.querySelector(".button");
+// Função para iniciar o contador em h1
+function iniciarContador1() {
+  var contadorElement = document.getElementById("contador1");
+  var contador = 0;
 
-btn.onmousemove = function (e) {
-  var x = e.pageX - btn.offsetLeft;
-  var y = e.pageY - btn.offsetTop;
+  // Definindo a função de incremento
+  function incrementar() {
+    contadorElement.textContent = contador;
+    contador++;
 
-  btn.style.setProperty("--eixoX", x + "px");
-  btn.style.setProperty("--eixoY", y + "px");
+    // Se contador for menor que 700, continue incrementando
+    if (contador <= 700) {
+      setTimeout(incrementar, 10); // Defina o intervalo de atualização aqui
+    }
+  }
+
+  // Iniciar o incremento
+  incrementar();
+}
+
+// Função para iniciar o contador em h2
+function iniciarContador2() {
+  var contadorElement = document.getElementById("contador2");
+  var contador = 0;
+
+  // Definindo a função de incremento
+  function incrementar() {
+    contadorElement.textContent = contador;
+    contador++;
+
+    // Se contador for menor que 700, continue incrementando
+    if (contador <= 90000) {
+      setTimeout(incrementar, 1); // Defina o intervalo de atualização aqui
+    }
+  }
+
+  // Iniciar o incremento
+  incrementar();
+}
+
+// Iniciar os contadores quando a página estiver carregada
+window.onload = function () {
+  iniciarContador1();
+  iniciarContador2();
 };
