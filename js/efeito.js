2e39905debdfead1c3f26282892ed99f94981092
albertomoiseisdev <contatoalbertomoiseis@gmail.com>
2024-03-06 12:07:22 -0300

    att

--- js/efeito.js
+++ js/efeito.js
@@ -9,7 +9,7 @@ function iniciarContador1() {
     contador++;
 
     // Se contador for menor que 700, continue incrementando
-    if (contador <= 700) {
+    if (contador <= 10) {
       setTimeout(incrementar, 10); // Defina o intervalo de atualização aqui
     }
   }
@@ -29,8 +29,48 @@ function iniciarContador2() {
     contador++;
 
     // Se contador for menor que 700, continue incrementando
-    if (contador <= 90000) {
-      setTimeout(incrementar, 1); // Defina o intervalo de atualização aqui
+    if (contador <= 8) {
+      setTimeout(incrementar, 10); // Defina o intervalo de atualização aqui
+    }
+  }
+
+  // Iniciar o incremento
+  incrementar();
+}
+
+// Função para iniciar o contador em h2
+function iniciarContador3() {
+  var contadorElement = document.getElementById("contador3");
+  var contador3 = 0;
+
+  // Definindo a função de incremento
+  function incrementar() {
+    contadorElement.textContent = contador3;
+    contador3++;
+
+    // Se contador for menor que 700, continue incrementando
+    if (contador3 <= 700) {
+      setTimeout(incrementar, 8); // Defina o intervalo de atualização aqui
+    }
+  }
+
+  // Iniciar o incremento
+  incrementar();
+}
+
+// Função para iniciar o contador em h2
+function iniciarContador4() {
+  var contadorElement = document.getElementById("contador4");
+  var contador4 = 0;
+
+  // Definindo a função de incremento
+  function incrementar() {
+    contadorElement.textContent = contador4;
+    contador4++;
+
+    // Se contador for menor que 700, continue incrementando
+    if (contador4 <= 90) {
+      setTimeout(incrementar, 8); // Defina o intervalo de atualização aqui
     }
   }
 
@@ -42,4 +82,6 @@ function iniciarContador2() {
 window.onload = function () {
   iniciarContador1();
   iniciarContador2();
+  iniciarContador3();
+  iniciarContador4();
 };
